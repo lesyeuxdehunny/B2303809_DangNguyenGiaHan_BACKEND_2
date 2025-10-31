77a1b7736daa5da1dacc9e2ad6828ca6df92f989
lesyeuxdehunny <giahandangnguyen251205@gmail.com>
2025-10-31 22:52:10 +0700

    Ket noi CDSL, cap nhat va tao cac dich vu moi co kem cac method

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,27 +1,9 @@
-exports.create = (req, res) => {
-    res.send({message: "create handler"});
-};
+const { ObjectId } = require('mongodb')
 
-exports.findAll = (req, res) => {
-    res.send({message: "findAll handler"});
-};
+class ContactService {
+    constructor(client) {
+        this.Contact = client.db().collection("contacts")
+    }
+}
 
-exports.findOne = (req, res) => {
-    res.send({message: "findOne handler"});
-};
-
-exports.update = (req, res) => {
-    res.send({message: "update handler"});
-};
-
-exports.delete = (req, res) => {
-    res.send({message: "delete handler"});
-};
-
-exports.deleteAll = (req, res) => {
-    res.send({message: "deleteAll handler"});
-};
-
-exports.findAllFavorite = (req, res) => {
-    res.send({message: "findAllFavorite handler"});
-};
+module.exports = ContactService
