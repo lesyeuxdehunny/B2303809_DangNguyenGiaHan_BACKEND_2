4a0d52b49f027d68b2d06635fec30c4a0422b0df
lesyeuxdehunny <giahandangnguyen251205@gmail.com>
2025-11-01 00:59:14 +0700

    Cap nhat tu BACKEND_1

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,9 +1,16 @@
-const { ObjectId } = require('mongodb')
+const ContactService = require("../services/contact.service");
+const MongoDB = require("../utils/mongodb.util");
+const ApiError = require("../api-error");
 
-class ContactService {
-    constructor(client) {
-        this.Contact = client.db().collection("contacts")
+//Create and Save a new Contact
+exports.create = async (req, res, next) => {
+    if(!req.body?.name)
+        return next(new ApiError(400, "Name can not be empty"));
+    try {
+        const contactService = new ContactService(MongoDB.client)
+        const doc = await contactService.create(req.body)
+        res.json(doc)
+    } catch (err) {
+        return next(new ApiError(500, "An error occurred while creating the contact"));
     }
-}
-
-module.exports = ContactService
+};
